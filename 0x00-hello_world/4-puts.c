e1adb37c8255ab6970cb29a3b1778d3ffb914505
Bophelo22 <tshepiso9602@gmail.com>
2023-09-07 00:25:49 -0700

    #include <stdio.h> /**  * main - is where the program will start to execute  * Return: 0 will be returned as a success Q

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - is where the program will start to execute
+ * Return: 0 will be returned as a success
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle,\n");
+return (0);
+}
