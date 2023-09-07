71908f0d8c078e886bb618d10c9485931f151e32
Bophelo22 <tshepiso9602@gmail.com>
2023-09-07 01:45:34 -0700

    6-size.c committed

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - is where the program will start to execute
+ * Return: 0 will be returned as a success
+*/
+int main(void)
+{
+    char a ;
+    int an;
+    long al;
+    long long all;
+    float f;
+    printf("Size of a char: %zu byte(s)\n", sizeof(a));
+    printf("Size of an int: %zu byte(s)\n", sizeof(an));
+    printf("Size of a int: %zu byte(s)\n", sizeof(al));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(all));
+    printf("Size of a float: %zu byte(s)\n", sizeof(f));
+    return (0);
+}
