8d8f3031fc213c7328c11f052406af581928af45
Bophelo22 <tshepiso9602@gmail.com>
2023-09-07 00:41:39 -0700

    5-pruintf.c committed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - is where the program will start to execute
+ * Return: 0 will be returned as a success
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
