4b28e67e77f6ab0af98420dad702d37aa2eb7deb
Bophelo22 <tshepiso9602@gmail.com>
2023-09-07 01:52:31 -0700

    6-size.c committed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,15 @@
 */
 int main(void)
 {
-    char a;
-    int an;
-    long int al;
-    long long int all;
-    float f;
-    printf("Size of a char: %zu byte(s)\n", sizeof(a));
-    printf("Size of an int: %zu byte(s)\n", sizeof(an));
-    printf("Size of a int: %zu byte(s)\n", sizeof(al));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(all));
-    printf("Size of a float: %zu byte(s)\n", sizeof(f));
-    return (0);
+char a;
+int an;
+long int al;
+long long int all;
+float f;
+printf("Size of a char: %zu byte(s)\n", sizeof(a));
+printf("Size of an int: %zu byte(s)\n", sizeof(an));
+printf("Size of a int: %zu byte(s)\n", sizeof(al));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(all));
+printf("Size of a float: %zu byte(s)\n", sizeof(f));
+return (0);
 }
