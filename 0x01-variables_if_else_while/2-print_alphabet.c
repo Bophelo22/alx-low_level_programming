8b1fd275dff3c2770a10dad1d575d979948b00b0
Bophelo22 <tshepiso9602@gmail.com>
2023-09-09 21:25:37 -0700

    2-print_alphabet.c committed

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - where the execution start
+ * Return: 0 for success
+*/
+int main(void)
+{
+for (char letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
