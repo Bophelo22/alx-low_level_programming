efbcec9c9b077c7852470e815eeacd0b1cc28ece
Bophelo22 <tshepiso9602@gmail.com>
2023-09-09 22:36:24 -0700

    5-print_numbers.c committed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the start of the execution
+ * Return: 0 for success
+*/
+int main(void)
+{
+for (int i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+}
