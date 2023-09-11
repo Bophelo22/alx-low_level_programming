c9c355f54b4c3e3a854f1513a64b403f99fe8789
Bophelo22 <tshepiso9602@gmail.com>
2023-09-10 21:56:53 -0700

    101-print_comb4.c committed

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - the start of the execution
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+int i = 0;
+for (; i <= 9; i++)
+{
+int j = i + 1;
+for (; j <= 9; j++)
+{
+int k = j + 1;
+for (9; k <= 9; k++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar('0' + k);
+if ((i != 9) || (j != 9) || (k != 9))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
