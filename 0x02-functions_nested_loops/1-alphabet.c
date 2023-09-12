014a525184a5259f3c278c0a8199bddf41759fda
Bophelo22 <tshepiso9602@gmail.com>
2023-09-12 01:55:23 -0700

    1-alphabet.c committed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * main - Write a function that prints the alphabet, in lowercase
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+print_alphabet(); /*call the function print_alphabet*/
+return (0);
+}
+/*function to print alphabets in ascending order*/
+void print_alphabet(void)
+{
+int i = 'a';
+for (; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
