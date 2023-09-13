922d08aa4c2034f1c6b16e277a6dfb2084d65292
Bophelo22 <tshepiso9602@gmail.com>
2023-09-13 05:08:57 -0700

    1-alphabet.c, 1-main.c committed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print alphabet
+ * @void: nothing to be used
+ * Return: this function returns nothing
+*/
+void print_alphabet(void)
+{
+char i = 'a';
+for (; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Write a function that prints the alphabet, in lowercase
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+print_alphabet(); /*call the function print_alphabet*/
+return (0);
+}
