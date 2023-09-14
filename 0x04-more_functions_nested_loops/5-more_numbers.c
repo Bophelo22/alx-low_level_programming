b209475064844807c4da9e013692d373fc29f84c
Bophelo22 <tshepiso9602@gmail.com>
2023-09-14 06:44:08 -0700

    5-more_numbers.c committed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * more_numbers - function that prints 10 times the numbers, from 0 to 14, followed by a new line.
+ * @void: nothing to be used
  * Return: this function returns nothing
  */
 void more_numbers(void)
