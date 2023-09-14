ac78f401d587e9caabf65a9bf1bafaa096afe5a1
Bophelo22 <tshepiso9602@gmail.com>
2023-09-13 18:25:07 -0700

    103-fibonacci.c committed

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,24 +2,23 @@
 /**
  * main -Even Liber Abbaci
  * Return: Always 0 for success
-*/
+ */
 int main(void)
 {
-int i;
-unsigned long int j, k, sum, next;
+long j, k, sum, next;
 j = 1;
 k = 2;
 sum = 0;
-for (i = 1; i <= 33; ++i)
+for (; j < 44000000; ++j)
 {
-if ((j < 44000000) && ((j % 2) == 0))
+if ((j % 2) == 0)
 {
-sum += j;
+sum = sum + j;
 }
 next = j + k;
+j = k;
 k = next;
 }
-putchar('0' + sum);
-putchar('\n');
+printf("%lu\n", sum);
 return (0);
 }
