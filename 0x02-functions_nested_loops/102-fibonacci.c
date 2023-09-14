df9fc4e48b6425ff0fd339237409f9d41458e8bc
Bophelo22 <tshepiso9602@gmail.com>
2023-09-13 17:51:12 -0700

    102-fibonacci.c committed

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print in main
+ * Return: Always 0 for success
+*/
+int main(void)
+{
+int count;
+long fb1, fb2, sum;
+fb1 = 0;
+fb2 = 1;
+sum = 0;
+for (count = 0; count < 50; count++)
+{
+sum = fb1 + fb2;
+printf("lu", sum);
+fb1 = fb2;
+fb2 = sum;
+if (count == 49)
+{
+printf("\n");
+}
+else
+{
+print(",");
+}
+}
+return (0);
+}
