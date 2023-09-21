4e502c329f2cbe629c474d18e68a80da26e21b4f
Bophelo22 <tshepiso9602@gmail.com>
2023-09-20 22:12:39 -0700

    2-strncpy.c committed

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _strncpy - copy a string
+* @dest: to be used
+* @src: to be used
+* @n: input value to be used
+**
+Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int j;
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+return (dest);
+}
