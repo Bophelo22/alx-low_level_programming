a05208c61bddcd44ca4ebf504560300d8d639cd6
Bophelo22 <tshepiso9602@gmail.com>
2023-10-03 03:00:31 -0700

    0-create_array.c committed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,10 @@ char *create_array(unsigned int size, char c)
 char *str;
 int i;
 i = 0;
-str = malloc(sizeof(*str) * size);
+str = malloc(sizeof(char) * size);
+if (size == 0 || str == NULL)
+return (NULL);
 for (; i < size; i++)
-{
 str[i] = c;
-}
 return (str);
 }
