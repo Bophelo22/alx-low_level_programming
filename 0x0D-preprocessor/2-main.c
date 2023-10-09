9d9f5361135088226173dfe0f9bcdb20c59d547e
Bophelo22 <tshepiso9602@gmail.com>
2023-10-09 15:42:41 +0000

    0x0D. C - Preprocessor files committed

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,23 @@
+#include "_putchar.c"
+/**
+ * main - program that prints the name of the
+ * file it was compiled from, followed by a new line.
+ * Return: 0 for success
+*/
+int main(void)
+{
+int i;
+/**
+* char fileName[] = __FILE__;
+* works the same way as the following one
+*/
+char *fileName;
+fileName = __FILE__;
+i = 0;
+for(; (fileName[i] != '\0'); i++)
+{
+_putchar(fileName[i]);
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_LIKE_MACRO_HEADER
+#define FUNCTION_LIKE_MACRO_HEADER
+
+#define ABS(X) ((X) > (0) ? (X) : -(X))
+
+#endif
