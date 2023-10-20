69bd9a46a41c34ba35b7e4865f8e825761c13969
Bophelo22 <tshepiso9602@gmail.com>
2023-10-20 02:15:34 +0000

    1-list_len.c file committed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,12 +4,13 @@
  * list_len - function that returns the number of
  * elements in a linked list_t list.
  * @h: pointer to the list_t list
+ * Return: the address of the new element, or NULL if it failed
  */
 size_t list_len(const list_t *h)
 {
 size_t len;
 len = 0;
-while (h != NULL)
+while (h)
 {
 len++;
 h = h->next;    
