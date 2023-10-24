03f9fc73300fb05edfe8c690be930e40c792d195
Bophelo22 <tshepiso9602@gmail.com>
2023-10-24 00:09:33 -0700

    9-insert_nodeint.c file committed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * insert_nodeint_at_index - function that inserts a new node at a given position.
  * @head: pointer to the first node in the list
@@ -11,9 +11,9 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int i;
-listint_t *ptr;
+listint_t *ptr,*current;
 i = 0;
-listint_t *current = *head;
+current = *head;
 ptr = malloc(sizeof(listint_t));
 if (!ptr || !head)
 {
